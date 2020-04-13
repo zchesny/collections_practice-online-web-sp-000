97c4689e0ac11da5468e3975ec5089d9ef77976d
Zoey Chesny <z.chesny@gmail.com>
2020-04-13 06:27:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+# def sort_array_char_count(array)
+# end
+#
+# def swap_elements(array)
+# end
+#
+# def swap_elements_from_to(array, index, destination_index)
+# end
+#
+# def reverse_array(array)
+# end
+#
+# def kesha_maker(array)
+# end
+#
+# def find_a(array)
+# end
+#
+# def sum_array(array)
+# end
+#
+# def add_s(array)
+# end
